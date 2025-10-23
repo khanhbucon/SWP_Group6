a1bd7a8958bb019bcfc64aacd40a7ab041566e45
khanhbucon <149470422+khanhbucon@users.noreply.github.com>
2025-10-23 11:28:40 +0700

    fix lỗi

---
+++ Mo_DataAccess/Services/SepayConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Mo_DataAccess.Services
+{
+    public class SepayConfig
+    {
+        public string ApiUrl { get; set; } = null!;
+        public string ApiKey { get; set; } = null!;
+        public string AccountNumber { get; set; } = null!;
+        public string BankCode { get; set; } = null!;
+        public string AccountName { get; set; } = null!;
+    }
+}
---
+++ Mo_Entities/ModelResponse/CreateDepositResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mo_Entities.ModelResponse
+{
+    public class CreateDepositResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public long? PaymentTransactionId { get; set; }
+        public string? QrCodeUrl { get; set; }
+        public string? BankAccount { get; set; }
+        public string? BankName { get; set; }
+        public string? AccountName { get; set; }
+        public decimal Amount { get; set; }
+        public string? TransferContent { get; set; }
+    }
+}
